d52f7e383e36a7eed67651454bbb04639947116f
Dongjin <dongqui9@gmail.com>
2025-01-23 18:56:57 +0900

    refactor: row 컴포넌트

--- src/app/components/Row/Row.tsx
+++ src/app/components/Row/Row.tsx
@@ -1,20 +1,22 @@
 import { PropsWithChildren } from "react";
 
-import styles from "./Row.module.scss";
 import classNames from "classnames/bind";
+import styles from "./Row.module.scss";
 
 const cx = classNames.bind(styles);
 
 interface Props {
   spaceBetween?: boolean;
+  gap?: 2 | 4;
 }
 
 export default function Row({
   children,
   spaceBetween,
+  gap,
 }: PropsWithChildren<Props>) {
   return (
-    <div className={cx("row", { "space-between": spaceBetween })}>
+    <div className={cx("row", `gap-${gap}`, { "space-between": spaceBetween })}>
       {children}
     </div>
   );
