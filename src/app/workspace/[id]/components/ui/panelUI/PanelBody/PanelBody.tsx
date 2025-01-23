b4d357271027488afac4a3cc9e6edf08e79abae3
Dongjin <dongqui9@gmail.com>
2025-01-23 18:54:50 +0900

    feat: Panel UI

---
+++ src/app/workspace/[id]/components/ui/panelUI/PanelBody/PanelBody.tsx
@@ -0,0 +1,10 @@
+import { PropsWithChildren } from "react";
+
+import classNames from "classnames/bind";
+import styles from "./PanelBody.module.scss";
+
+const cx = classNames.bind(styles);
+
+export default function PanelBody({ children }: PropsWithChildren) {
+  return <div className={cx("container")}>{children}</div>;
+}
