29ae3c2ac4a451c7dde6c4820c84894759f6e368
itsmesunky <itisp_06@naver.com>
2025-02-04 16:13:39 +0900

    test: 액션바 테스트 코드 추가

--- src/app/workspace/[id]/page.tsx
+++ src/app/workspace/[id]/page.tsx
@@ -1,6 +1,12 @@
 // 작업공간
+import ActionBar from './components/ActionBar/ActionBar'
 import MemoPanel from './components/MemoPanel/MemoPanel'
 
 export default function Page() {
-  return <MemoPanel />
+  return (
+    <>
+      <ActionBar />
+      <MemoPanel />
+    </>
+  )
 }
