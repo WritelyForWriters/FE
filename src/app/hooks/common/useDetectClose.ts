2ee0187bfe1606a558b2ab2c6cf1a9eda7890c70
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2025-02-05 18:38:25 +0900

    feat: 툴바 select-menu 외부 클릭시 닫히는 훅 구현

---
+++ src/app/hooks/common/useDetectClose.ts
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from 'react'
+
+const useDetectClose = (onClose: () => void) => {
+  const ref = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const onClick = (e: Event) => {
+      if (ref.current !== null && !ref.current.contains(e.target as Node)) {
+        onClose()
+      }
+    }
+
+    document.addEventListener('mousedown', onClick)
+
+    return () => {
+      document.addEventListener('mousedown', onClick)
+    }
+  }, [onClose])
+
+  return ref
+}
+
+export default useDetectClose
