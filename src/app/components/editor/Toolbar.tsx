1b6302ec0ad9d2b3901191a3c896eab8cd41bdd1
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2025-02-06 16:16:27 +0900

    design: 각 툴바 옵션에 맞는 위치에 select-menu 열리도록 마크업 수정

--- src/app/components/editor/Toolbar.tsx
+++ src/app/components/editor/Toolbar.tsx
@@ -46,89 +46,101 @@ export default function Toolbar({ editor }: ToolbarProps) {
   }, [])
 
   return (
-    <ul className={styles['bubble-menu']}>
+    <div className={styles['bubble-menu']}>
       {/* 텍스트 형식 툴바 */}
-      <button onClick={() => setIsOpen(true)}>
-        {getTextFormatOption()}
-        <IoIosArrowDown size={16} fill="#CCCCCC" />
-      </button>
+      <div>
+        <button onClick={() => setIsOpen(true)}>
+          {getTextFormatOption()}
+          <IoIosArrowDown size={16} fill="#CCCCCC" />
+        </button>
 
-      {isOpen && (
-        <div ref={textFormatSelectMenuRef} className={styles['select-menu']}>
-          <button
-            onClick={toggleText}
-            className={getActiveStyleClass(
-              !editor.isActive('blockquote') && !editor.isActive('heading'),
-            )}
-          >
-            본문
-          </button>
-          <button
-            onClick={toggleHeading}
-            className={getActiveStyleClass(editor.isActive('heading'))}
-          >
-            제목
-          </button>
-          <button
-            onClick={toggleBlockquote}
-            className={getActiveStyleClass(editor.isActive('blockquote'))}
-          >
-            인용
-          </button>
-        </div>
-      )}
+        {isOpen && (
+          <div ref={textFormatSelectMenuRef} className={styles['select-menu']}>
+            <button
+              onClick={toggleText}
+              className={getActiveStyleClass(
+                !editor.isActive('blockquote') && !editor.isActive('heading'),
+              )}
+            >
+              본문
+            </button>
+            <button
+              onClick={toggleHeading}
+              className={getActiveStyleClass(editor.isActive('heading'))}
+            >
+              제목
+            </button>
+            <button
+              onClick={toggleBlockquote}
+              className={getActiveStyleClass(editor.isActive('blockquote'))}
+            >
+              인용
+            </button>
+          </div>
+        )}
+      </div>
 
       {/* 정렬 툴바 */}
-      <button onClick={() => setIsIndentOption(true)}>
-        정렬
-        <IoIosArrowDown size={16} fill="#CCCCCC" />
-      </button>
+      <div>
+        <button onClick={() => setIsIndentOption(true)}>
+          정렬
+          <IoIosArrowDown size={16} fill="#CCCCCC" />
+        </button>
 
-      {isIndentOption && (
-        <div ref={indentSelectMenuRef} className={styles['select-menu']}>
+        {isIndentOption && (
+          <div ref={indentSelectMenuRef} className={styles['select-menu']}>
+            <button
+              onClick={indent}
+              className={getActiveStyleClass(editor.getAttributes('paragraph').indent)}
+            >
+              + 들여쓰기
+            </button>
+            <button onClick={outdent}>- 내어쓰기</button>
+          </div>
+        )}
+      </div>
+
+      {/* 텍스트 mark 툴바 */}
+      <div>
+        {' '}
+        <div className={styles['text-mark']}>
           <button
-            onClick={indent}
-            className={getActiveStyleClass(editor.getAttributes('paragraph').indent)}
+            onClick={() => editor.chain().focus().toggleBold().run()}
+            className={getActiveStyleClass(editor.isActive('bold'))}
           >
-            + 들여쓰기
+            B
           </button>
-          <button onClick={outdent}>- 내어쓰기</button>
+          <button
+            onClick={() => editor.chain().focus().toggleItalic().run()}
+            className={getActiveStyleClass(editor.isActive('italic'))}
+          >
+            I
+          </button>
+          <button
+            onClick={() => editor.chain().focus().toggleUnderline().run()}
+            className={getActiveStyleClass(editor.isActive('underline'))}
+          >
+            U
+          </button>
         </div>
-      )}
-
-      <div className={styles['text-mark']}>
-        <button
-          onClick={() => editor.chain().focus().toggleBold().run()}
-          className={getActiveStyleClass(editor.isActive('bold'))}
-        >
-          B
-        </button>
-        <button
-          onClick={() => editor.chain().focus().toggleItalic().run()}
-          className={getActiveStyleClass(editor.isActive('italic'))}
-        >
-          I
-        </button>
-        <button
-          onClick={() => editor.chain().focus().toggleUnderline().run()}
-          className={getActiveStyleClass(editor.isActive('underline'))}
-        >
-          U
-        </button>
       </div>
 
       <div className={styles.line}>{''}</div>
 
       {/* 메모 툴바 */}
-      <button>메모</button>
+      <div>
+        <button>메모</button>
+      </div>
 
       <div className={styles.line}>{''}</div>
 
       {/* AI 어시스턴트 */}
-      <button onClick={() => setIsIndentOption(true)}>
-        AI 어시스턴트
-        <IoIosArrowDown size={16} fill="#CCCCCC" />
-      </button>
-    </ul>
+      <div>
+        <button onClick={() => setIsIndentOption(true)}>
+          AI 어시스턴트
+          <IoIosArrowDown size={16} fill="#CCCCCC" />
+        </button>
+      </div>
+    </div>
   )
 }
