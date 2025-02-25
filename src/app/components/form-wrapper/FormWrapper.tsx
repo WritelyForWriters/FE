d70d5a6b42f4d251619a99a7281546a5888417be
HaJaehyeong <hajae305@gmail.com>
2025-02-25 11:51:02 +0900

    feat: add form-wrapper component

---
+++ src/app/components/form-wrapper/FormWrapper.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from 'react'
+
+import { DefaultValues, FieldValues, FormProvider, useForm } from 'react-hook-form'
+
+interface FormWrapperProps<T> {
+  children: ReactNode
+  onSubmit: (data: T) => void
+  defaultValues?: DefaultValues<T>
+}
+
+export default function FormWrapper<T extends FieldValues>({
+  children,
+  onSubmit,
+  defaultValues,
+}: FormWrapperProps<T>) {
+  const methods = useForm<T>({ defaultValues })
+
+  const handleSubmit = methods.handleSubmit(async (data: T) => {
+    onSubmit(data)
+  })
+
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit}>{children}</form>
+    </FormProvider>
+  )
+}
