cd441a5a7094eae505c5bd03e2b71d220fe33718
HaJaehyeong <hajae305@gmail.com>
2025-02-26 13:49:48 +0900

    feat: add checkbox input

--- src/app/components/checkbox/Checkbox.tsx
+++ src/app/components/checkbox/Checkbox.tsx
@@ -1,3 +1,47 @@
-export default function Checkbox() {
-  return <></>
+'use client'
+
+import Image from 'next/image'
+
+import { InputHTMLAttributes, useState } from 'react'
+
+import checkedIcon from '/public/icons/checked.svg'
+import uncheckedIcon from '/public/icons/unchecked.svg'
+
+import classNames from 'classnames/bind'
+
+import styles from './Checkbox.module.scss'
+
+const cx = classNames.bind(styles)
+
+interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: string
+  checked?: boolean
+}
+
+export default function Checkbox({ label, checked = false, ...props }: CheckboxProps) {
+  const [isChecked, setIsChecked] = useState(checked)
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(e.target.checked)
+
+    if (props.onChange) props.onChange(e)
+  }
+
+  return (
+    <section className={cx('checkbox')}>
+      {isChecked ? (
+        <Image src={checkedIcon.src} width={20} height={20} alt="unchecked-image" />
+      ) : (
+        <Image src={uncheckedIcon.src} width={20} height={20} alt="checked-image" />
+      )}
+      <input
+        className={cx('checkbox__input')}
+        type="checkbox"
+        checked={isChecked}
+        onChange={handleChange}
+        {...props}
+      />
+      <label className={cx('checkbox__label')}>{label}</label>
+    </section>
+  )
 }
