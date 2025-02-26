5a2d8d6bc142b8b1b0f085d126c46e17f50ef933
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-02-26 22:12:06 +0900

    feat: 공통 컴포넌트 - 액션바 구현

--- src/app/components/action-bar/ActionBar.tsx
+++ src/app/components/action-bar/ActionBar.tsx
@@ -1,13 +1,16 @@
 'use client'
 
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
+import { FormProvider, useForm } from 'react-hook-form'
 import { FiInfo } from 'react-icons/fi'
 import { MdHome } from 'react-icons/md'
 
 import FillButton from '@components/buttons/FillButton'
+import TextButton from '@components/buttons/TextButton'
+import Modal from '@components/modal/Modal'
 import SelectMenu from '@components/select-menu/SelectMenu'
-import EditModeSwitch from '@components/switch/EditModeSwitch/EditModeSwitch'
+import EditModeSwitch from '@components/switch/EditModeSwitch'
 
 import classNames from 'classnames/bind'
 
@@ -16,12 +19,21 @@ import styles from './ActionBar.module.scss'
 const cx = classNames.bind(styles)
 
 type usage = 'workspace' | 'planner'
+type exportMode = 'full' | 'toc'
 
 interface ActionBarProps {
   usage: usage
 }
 
+interface ModalHandler {
+  open: () => void
+  close: () => void
+}
+
 export default function ActionBar({ usage }: ActionBarProps) {
+  const methods = useForm()
+  const ref = useRef<ModalHandler | null>(null)
+
   // 저장 여부 판단 state
   const [hasSaved, setHasSaved] = useState(false)
 
@@ -33,6 +45,10 @@ export default function ActionBar({ usage }: ActionBarProps) {
 
   // 내보내기 버튼 클릭 여부 구분하는 state
   const [isExportMenuOpen, setIsExportMenuOpen] = useState(false)
+
+  // 내보내기 구분 state
+  // full: 전체 내보내기, toc: 목차 내보내기
+  const [exportMode, setExportMode] = useState<exportMode>()
 
   // 타이틀명 state
   const [title, setTitle] = useState('타이틀')
@@ -83,6 +99,20 @@ export default function ActionBar({ usage }: ActionBarProps) {
     }
   }
 
+  const handleModalClose = () => {
+    ref.current?.close()
+  }
+
+  const handleModalOpen = () => {
+    ref.current?.open()
+  }
+
+  const handleExportSelection = (exportMode: exportMode) => {
+    setExportMode(exportMode)
+    setIsExportMenuOpen(false)
+    handleModalOpen()
+  }
+
   // 저장하기 버튼 렌더링 조건: (작업공간 & 쓰기모드) || (작품 플래너 && 저장 전)
   const showSaveButton =
     (usage === 'workspace' && isContentEditing) || (usage === 'planner' && !hasSaved)
@@ -95,76 +125,119 @@ export default function ActionBar({ usage }: ActionBarProps) {
     (usage === 'workspace' && !isContentEditing) || (usage === 'planner' && hasSaved)
 
   return (
-    <div className={cx('.action-bar-wrapper')}>
-      <div className={cx('action-bar-inner-container')}>
-        <section className={cx('action-bar-action-section')}>
-          {/* 홈 버튼 */}
-          <button onClick={handleHomeClick}>
-            <MdHome size={24}></MdHome>
-          </button>
-          {showSaveButton && <button onClick={() => handleSave(usage)}>저장하기</button>}
-          {showExportButton && (
-            <div className={cx('export-button')}>
-              <button onClick={handleExport}>내보내기</button>
-              {/* 내보내기 옵션 */}
-              <SelectMenu handleClose={() => setIsExportMenuOpen(false)} isOpen={isExportMenuOpen}>
-                <SelectMenu.Option option={{ className: styles['select-option'] }}>
-                  작품 전체 내보내기
-                </SelectMenu.Option>
-                <SelectMenu.Option option={{ className: styles['select-option'] }}>
-                  목차 내보내기
-                </SelectMenu.Option>
-              </SelectMenu>
-            </div>
-          )}
-          {showDeleteButton && <button onClick={() => handleDelete(usage)}>삭제하기</button>}
-        </section>
-        <section className={cx('action-bar-info-section')}>
-          <section className={cx('title')}>
-            {isTitleEditing ? (
-              <input
-                className={cx('action-bar-input')}
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                onBlur={() => setIsTitleEditing(false)}
-                onKeyDown={handleKeyDown}
-              />
-            ) : (
-              <span className={cx('action-bar-span')} onClick={() => setIsTitleEditing(true)}>
-                {title}
-              </span>
+    <>
+      <Modal
+        ref={ref}
+        title="작품 전체 내보내기"
+        cancelText="취소"
+        confirmText="내보내기"
+        onCancel={handleModalClose}
+        onConfirm={handleExport}
+        subtitle="내보낼 파일의 형태와 형식을 지정해 주세요."
+        content={
+          <FormProvider {...methods}>
+            <form>
+              {/* 드롭다운 컴포넌트 머지 되면 반영 */}
+              <span>{exportMode === 'full' ? '작품 전체' : '목차'} 내보내기</span>
+            </form>
+          </FormProvider>
+        }
+      />
+      <div className={cx('action-bar-wrapper')}>
+        <div className={cx('action-bar-inner-container')}>
+          <section className={cx('action-bar-action-section')}>
+            {/* 홈 버튼 */}
+            <button onClick={handleHomeClick}>
+              <MdHome size={24}></MdHome>
+            </button>
+            {showSaveButton && (
+              <TextButton size="large" onClick={() => handleSave(usage)}>
+                저장하기
+              </TextButton>
             )}
-            {/* Note: description은 동적 렌더링 필요 */}
-            <span className={cx('description')}>저장 중</span>
+            {showExportButton && (
+              <div className={cx('export-button-wrapper')}>
+                <TextButton size="large" onClick={() => setIsExportMenuOpen(true)}>
+                  내보내기
+                </TextButton>
+                {/* 내보내기 옵션 */}
+                <SelectMenu
+                  handleClose={() => setIsExportMenuOpen(false)}
+                  isOpen={isExportMenuOpen}
+                >
+                  <SelectMenu.Option
+                    option={{
+                      handleAction: () => handleExportSelection('full'),
+                      className: styles['select-option'],
+                    }}
+                  >
+                    작품 전체 내보내기
+                  </SelectMenu.Option>
+                  <SelectMenu.Option
+                    option={{
+                      handleAction: () => handleExportSelection('toc'),
+                      className: styles['select-option'],
+                    }}
+                  >
+                    목차 내보내기
+                  </SelectMenu.Option>
+                </SelectMenu>
+              </div>
+            )}
+            {showDeleteButton && (
+              <TextButton size="large" onClick={() => handleDelete(usage)}>
+                삭제하기
+              </TextButton>
+            )}
           </section>
-        </section>
-        <section className={cx('action-bar-extra-action-section')}>
-          {usage === 'workspace' ? (
-            <>
-              <button>
-                <FiInfo size={20} color="#CCCCCC" />
-              </button>
-              <menu className={cx('action-bar-tabs')}>
-                <EditModeSwitch
-                  isSelected={!isContentEditing}
-                  onClick={() => setIsContentEditing(false)}
-                  disabled={!hasSaved}
-                >
-                  읽기 모드
-                </EditModeSwitch>
-                <EditModeSwitch
-                  isSelected={isContentEditing}
-                  onClick={() => setIsContentEditing(true)}
-                >
-                  쓰기 모드
-                </EditModeSwitch>
-              </menu>
-            </>
-          ) : (
-            <FillButton size="large">집필하러 가기</FillButton>
-          )}
-        </section>
+          <section className={cx('action-bar-info-section')}>
+            <section className={cx('title')}>
+              {isTitleEditing ? (
+                <input
+                  className={cx('action-bar-input')}
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  onBlur={() => setIsTitleEditing(false)}
+                  onKeyDown={handleKeyDown}
+                />
+              ) : (
+                <span className={cx('action-bar-span')} onClick={() => setIsTitleEditing(true)}>
+                  {title}
+                </span>
+              )}
+              {/* Note: description은 동적 렌더링 필요 */}
+              <span className={cx('description')}>저장 중</span>
+            </section>
+          </section>
+          <section className={cx('action-bar-extra-action-section')}>
+            {usage === 'workspace' ? (
+              <>
+                {/* Note: 해당 버튼 클릭 시 도움말 메뉴 오픈 */}
+                <button>
+                  <FiInfo size={20} color="#CCCCCC" />
+                </button>
+                <menu className={cx('action-bar-tabs')}>
+                  <EditModeSwitch
+                    isSelected={!isContentEditing}
+                    onClick={() => setIsContentEditing(false)}
+                    disabled={!hasSaved}
+                  >
+                    읽기 모드
+                  </EditModeSwitch>
+                  <EditModeSwitch
+                    isSelected={isContentEditing}
+                    onClick={() => setIsContentEditing(true)}
+                  >
+                    쓰기 모드
+                  </EditModeSwitch>
+                </menu>
+              </>
+            ) : (
+              <FillButton size="large">집필하러 가기</FillButton>
+            )}
+          </section>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
