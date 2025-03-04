c698bce1c49e23dd7c342c442bf98eee74e47969
HaJaehyeong <hajae305@gmail.com>
2025-03-04 14:51:54 +0900

    refactor: modify error message

--- src/app/components/toast/ToastProvider.tsx
+++ src/app/components/toast/ToastProvider.tsx
@@ -15,7 +15,7 @@ const ToastContext = createContext<ToastContextType | undefined>(undefined)
 export function useToast() {
   const context = useContext(ToastContext)
   if (!context) {
-    throw new Error('useToast must be used within a ToastProvider')
+    throw new Error('useToast는 ToastProvider 내부에서 실행되어야 합니다.')
   }
   return context.showToast
 }
