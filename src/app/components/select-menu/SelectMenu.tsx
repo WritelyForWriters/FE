0e19c5b500b9c3af766b21c817e5aeffad982a56
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2025-03-05 23:35:07 +0900

    design: select-menu 글로벌 스타일 적용

--- src/app/components/select-menu/SelectMenu.tsx
+++ src/app/components/select-menu/SelectMenu.tsx
@@ -1,10 +1,14 @@
-import { ReactNode } from 'react'
+import { CSSProperties, ReactNode } from 'react'
 
 import { SelectOptionType } from 'types/common/selectMenu'
 
 import { useDetectClose } from '@hooks/index'
 
+import classNames from 'classnames/bind'
+
 import styles from './SelectMenu.module.scss'
+
+const cx = classNames.bind(styles)
 
 interface SelectOptionProps {
   option: SelectOptionType
@@ -27,15 +31,16 @@ interface SelectMenuProps {
   children: ReactNode
   isOpen: boolean
   handleClose: () => void
+  style?: CSSProperties
 }
 
-export default function SelectMenu({ isOpen, handleClose, children }: SelectMenuProps) {
+export default function SelectMenu({ isOpen, handleClose, children, style }: SelectMenuProps) {
   const selectMenuRef = useDetectClose(handleClose)
 
   return (
     <>
       {isOpen && (
-        <div ref={selectMenuRef} className={styles['select-menu']}>
+        <div ref={selectMenuRef} className={cx('select-menu')} style={style}>
           {children}
         </div>
       )}
