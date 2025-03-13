7e8b82cbc6184e72cfaa2274b18091bbfce8dd78
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2025-03-13 21:57:42 +0900

    KAN-32 feat: 외부에서 에디터 인스턴스에 접근하기 위해 ref 사용

--- src/app/workspace/[id]/page.tsx
+++ src/app/workspace/[id]/page.tsx
@@ -1,3 +1,9 @@
+'use client'
+
+import { useRef } from 'react'
+
+import { Editor } from '@tiptap/react'
+
 import DefaultEditor from '@components/editor/DefaultEditor'
 
 import MemoPannel from './components/memo-pannel/MemoPannel'
@@ -13,16 +19,31 @@ const cx = classNames.bind(styles)
 /**
  * MEMO(Sohyun) 목차 패널, 메모 패널은 에디터와 밀접하게 연결되어 있으므로 하위 컴포넌트로 묶어서 분리할 필요성 고민
  */
+
+export interface RefEditor {
+  getEditor: () => Editor | null
+}
+
 export default function WorkSpacePage() {
+  const editorRef = useRef<RefEditor>(null)
+
+  const handleSave = async () => {
+    if (editorRef.current) {
+      const editor = editorRef.current.getEditor()
+      console.log(editor)
+      // TODO 에디터 저장로직
+    }
+  }
+
   return (
     <div className={cx('container')}>
-      <WorkspaceActionBar />
+      <WorkspaceActionBar onClickSave={handleSave} />
 
       <main className={cx('main-section')}>
         <MemoPannel />
 
         <div className={cx('main-section__editor')}>
-          <DefaultEditor />
+          <DefaultEditor ref={editorRef} />
         </div>
 
         <div className={cx('main-section__pannel')}>
