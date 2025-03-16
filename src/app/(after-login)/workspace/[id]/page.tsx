b30b31f2940870a63459889435ab1d4323683555
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2025-03-16 16:00:10 +0900

    KAN-32 feat: 목차 패널 공통 컴포넌트로 분리

--- src/app/(after-login)/workspace/[id]/page.tsx
+++ src/app/(after-login)/workspace/[id]/page.tsx
@@ -5,8 +5,8 @@ import { useRef } from 'react'
 import { HandleEditor } from 'types/common/editor'
 
 import DefaultEditor from '@components/editor/DefaultEditor'
+import IndexPannel from '@components/pannel/IndexPannel'
 
-import IndexPannel from './_components/index-pannel/IndexPannel'
 import MemoPannel from './_components/memo-pannel/MemoPannel'
 import PlannerPannel from './_components/planner-pannel/PlannerPannel'
 import WorkspaceActionBar from './_components/workspace-action-bar/WorkspaceActionBar'
@@ -16,6 +16,14 @@ import classNames from 'classnames/bind'
 import styles from '../../layout.module.scss'
 
 const cx = classNames.bind(styles)
+
+// mock data example
+const TABLE_OF_CONTENTS = [
+  { id: 'heading1', title: '제목 1' },
+  { id: 'heading2', title: '제목 2' },
+  { id: 'heading3', title: '제목 3' },
+  { id: 'heading4', title: '제목 4' },
+]
 
 export default function WorkSpacePage() {
   const editorRef = useRef<HandleEditor>(null)
@@ -34,7 +42,8 @@ export default function WorkSpacePage() {
       <div className={cx('header-space')}></div>
 
       <main className={cx('main-section')}>
-        <IndexPannel />
+        {/* TODO ToC 데이터를 IndexPannel로 전달 */}
+        <IndexPannel toc={TABLE_OF_CONTENTS} />
 
         <div className={cx('index-space')}></div>
 
