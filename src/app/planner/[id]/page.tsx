2b0c887aa79abcb3454877399d4cabaf8a398e66
HaJaehyeong <hajae305@gmail.com>
2025-03-17 17:09:59 +0900

    KAN-103 refactor: add constants.ts for planner page

---
+++ src/app/planner/[id]/page.tsx
@@ -0,0 +1,22 @@
+import { PLANNER_TABS, PlannerTabType } from 'constants/planner/plannerConstants'
+
+import PlannerActionBar from './components/planner-action-bar/PlannerActionBar'
+import PlannerTab from './components/planner-tab/PlannerTab'
+
+type Params = Promise<{ id: string }>
+type SearchParams = Promise<{ tab?: string }>
+
+export default async function PlannerPage(props: { params: Params; searchParams: SearchParams }) {
+  // TODO(hajae): for data fetch
+  // const id = (await props.params).id
+  const tab = (await props.searchParams).tab || 'synopsys'
+  const selectedTab: PlannerTabType =
+    PLANNER_TABS.find((plannerTab) => plannerTab.value === tab) || PLANNER_TABS[0]
+
+  return (
+    <div>
+      <PlannerActionBar />
+      <PlannerTab selectedTab={selectedTab} />
+    </div>
+  )
+}
