4161184c9856f57b9020ee5106a30d7ee96dad8e
HaJaehyeong <hajae305@gmail.com>
2025-03-20 08:38:58 +0900

    KAN-103 feat: add component > planner character form and list

--- src/app/(after-login)/planner/[id]/_components/planner-synopsis-form-container/PlannerSynopsisFormContainer.tsx
+++ src/app/(after-login)/planner/[id]/_components/planner-synopsis-form-container/PlannerSynopsisFormContainer.tsx
@@ -3,6 +3,7 @@
 import FormWrapper from '@components/form-wrapper/FormWrapper'
 
 import { PlannerSynopsisFormValue } from '../../types/plannerSynopsisFormValue'
+import PlannerCharacterForm from '../planner-character-form/PlannerCharacterForm'
 import PlannerSynopsisForm from '../planner-synopsis-form/PlannerSynopsisForm'
 import PlannerWorldViewForm from '../planner-world-view-form/PlannerWorldViewForm'
 
@@ -22,6 +23,7 @@ export default function PlannerSynopsisFormContainer() {
     >
       <PlannerSynopsisForm />
       <PlannerWorldViewForm />
+      <PlannerCharacterForm />
       <button type="submit">Submit</button>
     </FormWrapper>
   )
