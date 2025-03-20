5101e7ca8e9b770a99b92d8e534dfa7d11cbc097
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-03-20 09:11:20 +0900

    KAN-92 refactor: change outlined-button prop type

--- src/app/components/buttons/OutLinedButton.tsx
+++ src/app/components/buttons/OutLinedButton.tsx
@@ -2,7 +2,7 @@
  *  버튼 공통 컴포넌트 - OutLined Button
  * @author 선우
  */
-import { ReactNode } from 'react'
+import { ButtonPropsBase } from 'types/common/button'
 
 import classNames from 'classnames/bind'
 
@@ -10,18 +10,11 @@ import styles from './OutLinedButton.module.scss'
 
 const cx = classNames.bind(styles)
 
-interface OutLinedButtonProps {
-  size: 'large'
-  shape?: 'square'
-  variant?: 'primary'
-  children: ReactNode
-}
-
 export default function OutLinedButton({
   size,
   shape = 'square',
   variant = 'primary',
   children,
-}: OutLinedButtonProps) {
+}: ButtonPropsBase) {
   return <button className={cx('outlined-button', size, shape, variant)}>{children}</button>
 }
