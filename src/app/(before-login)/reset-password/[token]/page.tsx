26359168e57fb74bcd9a295008a53dd546926a52
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-03-20 17:00:13 +0900

    KAN-119 feat: 비밀번호 변경 페이지 구현

---
+++ src/app/(before-login)/reset-password/[token]/page.tsx
@@ -0,0 +1,96 @@
+'use client'
+
+/**
+ * 비밀번호 변경 페이지
+ * @author 선우
+ */
+import { useParams } from 'next/navigation'
+
+import { FormProvider, useForm } from 'react-hook-form'
+
+import FillButton from '@components/buttons/FillButton'
+import TextField from '@components/text-field/TextField'
+
+import classNames from 'classnames/bind'
+
+import styles from './page.module.scss'
+
+const cx = classNames.bind(styles)
+
+interface ResetPasswordFormValues {
+  password: string
+  confirmPassword: string
+}
+
+export default function ResetPassword() {
+  const params = useParams()
+
+  const methods = useForm<ResetPasswordFormValues>({
+    mode: 'onBlur',
+    defaultValues: {
+      password: '',
+      confirmPassword: '',
+    },
+  })
+
+  const { handleSubmit, trigger, getValues } = methods
+
+  const changePasswordToken = params?.token
+
+  const handleChangePassword = (data: ResetPasswordFormValues) => {
+    const password = data.password
+    console.log(`changePasswordToken: ${changePasswordToken}, password: ${password}`)
+  }
+
+  return (
+    <main className={cx('wrapper')}>
+      <FormProvider {...methods}>
+        <form className={cx('form')} onSubmit={handleSubmit(handleChangePassword)}>
+          <section className={cx('form__input-section')}>
+            <h3 className={cx('form__input-section__title')}>
+              새로운 비밀번호를
+              <br />
+              입력해 주세요
+            </h3>
+            <div className={cx('form__input-section__inputs')}>
+              <TextField
+                name="password"
+                label="새 비밀번호"
+                variant="password"
+                options={{
+                  onChange: () => {
+                    if (getValues('confirmPassword')) {
+                      trigger('confirmPassword')
+                    }
+                  },
+                  // TODO: 타 브랜치 머지되면 수정
+                  pattern: {
+                    value:
+                      /^(?=(.*[A-Z]))(?=(.*[a-z]))(?=(.*\d))(?=(.*[\W_])).{8,}$|^(?=(.*[A-Z]))(?=(.*[a-z]))(?=(.*\d)).{8,}$|^(?=(.*[A-Z]))(?=(.*[a-z]))(?=(.*[\W_])).{8,}$|^(?=(.*[A-Z]))(?=(.*\d))(?=(.*[\W_])).{8,}$|^(?=(.*[a-z]))(?=(.*\d))(?=(.*[\W_])).{8,}$/,
+                    message:
+                      '비밀번호는 최소 8자 이상으로, 대문자, 소문자, 숫자, 특수문자 중 3가지를 포함해야 해요.',
+                  },
+                }}
+              />
+              <TextField
+                name="confirmPassword"
+                label="새 비밀번호 확인"
+                variant="password"
+                options={{
+                  validate: (value) => {
+                    if (value !== getValues('password')) {
+                      return '비밀번호가 일치하지 않습니다.'
+                    }
+                  },
+                }}
+              />
+            </div>
+          </section>
+          <FillButton type="submit" size="large">
+            확인
+          </FillButton>
+        </form>
+      </FormProvider>
+    </main>
+  )
+}
