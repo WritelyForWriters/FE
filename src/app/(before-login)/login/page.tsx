e3bd2d0f92a70c45770983e7535d5a7c2f3bf8d0
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-03-28 12:04:35 +0900

    KAN-93 feat: 로그인 api 연동

--- src/app/(before-login)/login/page.tsx
+++ src/app/(before-login)/login/page.tsx
@@ -4,15 +4,23 @@
  * 로그인 페이지
  * @author 선우
  */
+import { useRouter } from 'next/navigation'
+
+import { useEffect } from 'react'
+
+import { setCookie } from 'cookies-next'
+import { useAtomValue } from 'jotai'
 import { FormProvider, useForm } from 'react-hook-form'
 import { login } from 'service/auth/auth'
-import { LoginFormFieldValues } from 'types/auth/join'
+import { isLoggedInAtom } from 'store/isLoggedInAtom'
+import { LoginFormFieldValues } from 'types/auth/login'
 
 import FillButton from '@components/buttons/FillButton'
 import OutLinedButton from '@components/buttons/OutLinedButton'
 import TextButton from '@components/buttons/TextButton'
 import Checkbox from '@components/checkbox/Checkbox'
 import TextField from '@components/text-field/TextField'
+import { useToast } from '@components/toast/ToastProvider'
 
 import classNames from 'classnames/bind'
 
@@ -21,20 +29,58 @@ import styles from './page.module.scss'
 const cx = classNames.bind(styles)
 
 export default function LoginPage() {
+  const router = useRouter()
+  const showToast = useToast()
+
   const methods = useForm<LoginFormFieldValues>()
 
   const { handleSubmit } = methods
 
+  const isLoggedIn = useAtomValue(isLoggedInAtom)
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      window.location.href = '/'
+    }
+  }, [isLoggedIn])
+
   // 로그인 하기
   const handleLogin = async (data: LoginFormFieldValues) => {
-    const { email, password } = data
+    const { email, password, rememberMe } = data
 
-    const result = await login({ email, password })
-    console.log(result)
+    try {
+      const response = await login({ email, password })
+
+      if (response.code === 'RESULT-001') {
+        // TODO: cookie expire 의사 결정 필요
+        const date = new Date()
+        date.setTime(date.getTime() + 60 * 60 * 1000)
+
+        if (rememberMe) {
+          setCookie('isRemberMe', true, { expires: date, path: '/' })
+        }
+
+        setCookie(
+          'refreshToken',
+          response.result.refreshToken,
+          rememberMe ? { expires: date, path: '/' } : {},
+        )
+        setCookie('isLoggedIn', true, rememberMe ? { expires: date, path: '/' } : {})
+
+        router.replace('/')
+      } else {
+        showToast('warning', response.message)
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        showToast('warning', error.message)
+      }
+    }
   }
 
-  // 로그인 유지하기
-  const handleRememberMe = async () => {}
+  const navigateTo = (url: string) => {
+    router.push(url)
+  }
 
   return (
     <main className={cx('wrapper')}>
@@ -61,7 +107,7 @@ export default function LoginPage() {
                 }}
               />
               <div className={cx('login-form__checkbox-section')}>
-                <Checkbox label="로그인 유지하기" name="rememberMe" onChange={handleRememberMe} />
+                <Checkbox label="로그인 유지하기" name="rememberMe" />
               </div>
             </form>
           </FormProvider>
@@ -70,11 +116,11 @@ export default function LoginPage() {
           <FillButton type="submit" size="large" onClick={handleSubmit(handleLogin)}>
             로그인
           </FillButton>
-          <OutLinedButton type="button" size="large">
+          <OutLinedButton type="button" size="large" onClick={() => navigateTo('/join')}>
             회원가입
           </OutLinedButton>
           <div className={cx('btn-section__find-password')}>
-            <TextButton type="button" size="small">
+            <TextButton type="button" size="small" onClick={() => navigateTo('/find-password')}>
               비밀번호 찾기
             </TextButton>
           </div>
