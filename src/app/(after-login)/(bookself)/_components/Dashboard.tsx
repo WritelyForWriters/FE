8a08b6522ef7dfe1c9af7302b155322c3f4ff6a3
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2025-03-28 17:31:04 +0900

    KAN-107 feat: 작품 저장 시 내 서재 페이지 대시보드 조회 로직 수정

--- src/app/(after-login)/(bookself)/_components/Dashboard.tsx
+++ src/app/(after-login)/(bookself)/_components/Dashboard.tsx
@@ -1,7 +1,7 @@
 import { getMeProfile } from 'services/members/members'
 import { ProductDto } from 'types/products'
 
-import CardItem from './CardItem'
+import CardList from './CardList'
 
 import classNames from 'classnames/bind'
 
@@ -23,11 +23,7 @@ export default async function Dashboard({ productList }: DashboardProps) {
           <h1 className={cx('dashboard__title')}>
             {`${profile?.nickname} 님, 오늘도 집필을 시작해볼까요?`}
           </h1>
-          <ul className={cx('dashboard__contents')}>
-            {productList.map((item) => (
-              <CardItem key={item.id} item={item} />
-            ))}
-          </ul>
+          <CardList productList={productList} />
         </div>
       ) : (
         <div>아직 작품이 없어요</div>
