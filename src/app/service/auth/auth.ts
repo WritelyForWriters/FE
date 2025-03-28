89276da60e8751400d1785cc346064df96edae36
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-03-28 12:02:40 +0900

    KAN-93 feat: api 호출 함수 추가

--- src/app/service/auth/auth.ts
+++ src/app/service/auth/auth.ts
@@ -1,11 +1,35 @@
-import { JoinFormData, LoginFormData } from 'types/auth/join'
+import { TOAST_MESSAGE } from 'constants/common/toastMessage'
+import { JoinFormData } from 'types/auth/join'
+import { LoginFormData } from 'types/auth/login'
 
-const url = process.env.NEXT_PUBLIC_API_URL
+const API_URL = process.env.NEXT_PUBLIC_API_URL
+
+// AT 갱신
+// TODO: API 수정 후 RT 관련 코드 삭제
+export const refreshAccessToken = async (refreshToken: string) => {
+  const res = await fetch(`${API_URL}/auth/token/reissue`, {
+    method: 'POST',
+    body: JSON.stringify({
+      refreshToken,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+
+  const data = await res.json()
+
+  if (data.code === 'RESULT-001') {
+    return data.result
+  } else {
+    throw new Error()
+  }
+}
 
 // 중복 확인
 export const checkValueDuplicate = async (type: 'email' | 'nickname', value: string) => {
   try {
-    const res = await fetch(`${url}/auth/check-${type}?${type}=${value}`, {
+    const res = await fetch(`${API_URL}/auth/check-${type}?${type}=${value}`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -23,7 +47,7 @@ export const checkValueDuplicate = async (type: 'email' | 'nickname', value: str
 // 회원가입
 export const join = async (formData: JoinFormData) => {
   try {
-    const res = await fetch(`${url}/auth/join`, {
+    const res = await fetch(`${API_URL}/auth/join`, {
       method: 'POST',
       body: JSON.stringify(formData),
       headers: {
@@ -33,16 +57,18 @@ export const join = async (formData: JoinFormData) => {
 
     const data = await res.json()
 
-    if (data.code === 'RESULT-001') {
-      return true
-    }
-  } catch {}
+    return data
+  } catch {
+    const error = new Error()
+    error.message = TOAST_MESSAGE.NETWORK_ERROR
+    throw error
+  }
 }
 
 // 로그인
 export const login = async (formData: LoginFormData) => {
   try {
-    const res = await fetch(`${url}/auth/login`, {
+    const res = await fetch(`${API_URL}/auth/login`, {
       method: 'POST',
       body: JSON.stringify(formData),
       headers: {
@@ -52,8 +78,10 @@ export const login = async (formData: LoginFormData) => {
 
     const data = await res.json()
 
-    if (data.code === 'RESULT-001') {
-      return true
-    }
-  } catch {}
+    return data
+  } catch {
+    const error = new Error()
+    error.message = TOAST_MESSAGE.NETWORK_ERROR
+    throw error
+  }
 }
