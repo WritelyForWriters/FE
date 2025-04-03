01e9315731ef3133d1db448c33f370ddf5f4b80d
HaJaehyeong <hajae305@gmail.com>
2025-04-03 22:56:32 +0900

    KAN-103 feat: image resize

--- src/app/components/editor/planner-idea-note-editor/PlannerIdeaNoteEditor.tsx
+++ src/app/components/editor/planner-idea-note-editor/PlannerIdeaNoteEditor.tsx
@@ -6,6 +6,7 @@ import Bold from '@tiptap/extension-bold'
 import Document from '@tiptap/extension-document'
 import Heading from '@tiptap/extension-heading'
 import Highlight from '@tiptap/extension-highlight'
+import Image from '@tiptap/extension-image'
 import Italic from '@tiptap/extension-italic'
 import Paragraph from '@tiptap/extension-paragraph'
 import Placeholder from '@tiptap/extension-placeholder'
@@ -13,8 +14,10 @@ import Text from '@tiptap/extension-text'
 import TextAlign from '@tiptap/extension-text-align'
 import Underline from '@tiptap/extension-underline'
 import { BubbleMenu, EditorContent, useEditor } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
 import { useAtom, useAtomValue, useSetAtom } from 'jotai'
 import { activeMenuAtom, isEditableAtom, selectionAtom } from 'store/editorAtoms'
+import ImageResize from 'tiptap-extension-resize-image'
 import { HandleEditor } from 'types/common/editor'
 
 import BlockquoteExtension from '@extensions/Blockquote'
@@ -61,6 +64,12 @@ export default function PlannerIdeaNoteEditor({ ref, contents }: PlannerIdeaNote
       Placeholder.configure({
         placeholder: '아이디어를 자유롭게 입력해주세요',
         emptyEditorClass: plannerStyles['is-editor-empty'],
+      }),
+      StarterKit,
+      ImageResize,
+      Image.configure({
+        inline: true,
+        allowBase64: true,
       }),
     ],
     immediatelyRender: false,
