911f4ee1593da2c52802099ddbb5125287115c2d
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-04-11 17:04:48 +0900

    KAN-134 refactor: refactor out-lined-button

--- src/app/components/buttons/OutLinedButton.tsx
+++ src/app/components/buttons/OutLinedButton.tsx
@@ -2,6 +2,8 @@
  *  버튼 공통 컴포넌트 - OutLined Button
  * @author 선우
  */
+import { useAtomValue } from 'jotai'
+import { clickedButtonAtom } from 'store/clickedButtonAtom'
 import { ButtonPropsBase } from 'types/common/button'
 
 import classNames from 'classnames/bind'
@@ -14,12 +16,31 @@ export default function OutLinedButton({
   size,
   shape = 'square',
   variant = 'primary',
+  iconPosition,
+  iconType,
   children,
   ...rest
 }: ButtonPropsBase) {
+  const clickedButton = useAtomValue(clickedButtonAtom)
+
+  const { name } = rest
+
+  const isActive = clickedButton === name
+
   return (
-    <button className={cx('outlined-button', size, shape, variant)} {...rest}>
-      {children}
+    <button
+      className={cx(
+        'outlined-button',
+        { 'outlined-button--active': isActive },
+        size,
+        shape,
+        variant,
+      )}
+      {...rest}
+    >
+      {iconType && iconPosition === 'leading' && iconType}
+      {iconType && iconPosition === 'only' ? iconType : children}
+      {iconType && iconPosition === 'trailing' && iconType}
     </button>
   )
 }
