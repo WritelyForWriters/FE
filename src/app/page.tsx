d9f5e1fca2ec26ae8ad5a7474d8edfabc1938ef9
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-04-11 17:06:40 +0900

    KAN-134 feat: add test code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
-import Dashboard from '(after-login)/(bookself)/_components/Dashboard'
-import MainHeader from '(after-login)/(bookself)/_components/MainHeader'
-import { getProductList } from 'services/products/products'
+'use client'
+
+import { useSetAtom } from 'jotai'
+import { selectedRangeAtom } from 'store/selectedRangeAtom'
+
+import ChatbotChatInput from '@components/chatbot-chat-input/ChatbotChatInput'
 
 /**
  * TODO
@@ -9,13 +12,37 @@ import { getProductList } from 'services/products/products'
  * [ ] 로그인 여부에 따른 버튼 UI -> 로그인 기능 완료 후
  */
 
-export default async function Home() {
-  const productList = await getProductList()
+export default function Home() {
+  const setSelectedRange = useSetAtom(selectedRangeAtom)
 
   return (
-    <div>
-      <MainHeader productCount={productList?.length} />
-      <Dashboard productList={productList} />
+    <div
+      style={{
+        width: 356,
+        height: 600,
+        margin: '0 auto',
+      }}
+    >
+      <p onMouseUp={() => setSelectedRange(window.getSelection()?.toString())}>
+        동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리 화려강산
+        대한사람 대한으로 길이 보전하세동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라
+        만세 무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세동해물과 백두산이 마르고 닳도록
+        하느님이 보우하사 우리나라 만세 무궁화 삼천리 화려강산 대한사람 대한으로 길이
+        보전하세동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리
+        화려강산 대한사람 대한으로 길이 보전하세동해물과 백두산이 마르고 닳도록 하느님이 보우하사
+        우리나라 만세 무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세동해물과 백두산이 마르고
+        닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리 화려강산 대한사람 대한으로 길이
+        보전하세동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리
+        화려강산 대한사람 대한으로 길이 보전하세동해물과 백두산이 마르고 닳도록 하느님이 보우하사
+        우리나라 만세 무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세동해물과 백두산이 마르고
+        닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리 화려강산 대한사람 대한으로 길이
+        보전하세동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리
+        화려강산 대한사람 대한으로 길이 보전하세동해물과 백두산이 마르고 닳도록 하느님이 보우하사
+        우리나라 만세 무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세동해물과 백두산이 마르고
+        닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리 화려강산 대한사람 대한으로 길이
+        보전하세
+      </p>
+      <ChatbotChatInput />
     </div>
   )
 }
