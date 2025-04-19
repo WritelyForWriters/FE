723938fe365fc61f381ced7252592f027aee0465
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2025-04-19 16:26:51 +0900

    KAN-126 feat: 제목이 없는 경우(빈 경우) 필터링 조건 추가

--- src/app/utils/getTocFromEditor.ts
+++ src/app/utils/getTocFromEditor.ts
@@ -14,6 +14,7 @@ export const getTocFromEditor = (node: JSONContent) => {
       if (node.type === 'heading' && node.attrs?.id) {
         // 서식, 줄바꿈이 적용된 제목의 경우 content가 배열로 저장되므로 합쳐서 하나의 문자열로 저장되도록
         const title = node.content?.map((value) => value.text).join('') || ''
+        if (!title.trim()) return // 제목이 없는 경우
         toc.push({ id: node.attrs.id, title })
       }
     })
