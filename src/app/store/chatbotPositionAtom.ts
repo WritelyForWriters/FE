d9a47ffedbd1da4964de557ba90c864ae2ed2f0b
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-04-23 17:57:59 +0900

    KAN-136 feat: 전역 상태 추가

---
+++ src/app/store/chatbotPositionAtom.ts
@@ -0,0 +1,4 @@
+import { atom } from 'jotai'
+
+// 챗봇 윈도우 position atom
+export const chatbotPositionAtom = atom({ x: 0, y: 0 })
---
+++ src/app/store/faviconRelativePosition.ts
@@ -0,0 +1,4 @@
+import { atom } from 'jotai'
+
+// favicon position atom
+export const favicionPositionAtom = atom({ xRatio: 0.9, yRatio: 0.9 })
