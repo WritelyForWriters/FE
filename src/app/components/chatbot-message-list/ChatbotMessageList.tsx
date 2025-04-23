4f1047991bc0657000927e746eced1cf0c148509
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-04-23 17:57:17 +0900

    KAN-136 style: 파일명 변경

--- src/app/components/chatbot-message-list/ChatbotMessageList.tsx
+++ src/app/components/chatbot-message-list/ChatbotMessageList.tsx
@@ -2,13 +2,13 @@ import ChatbotChatItems from '@components/chatbot-chat-items/ChatbotChatItems'
 
 import classNames from 'classnames/bind'
 
-import styles from './Chatbot.module.scss'
+import styles from './ChatbotMessageList.module.scss'
 
 const cx = classNames.bind(styles)
 
-export default function Chatbot() {
+export default function ChatbotMessageList() {
   return (
-    <ul className={cx('chatbot')}>
+    <ul className={cx('chatbot-message-list')}>
       <ChatbotChatItems />
     </ul>
   )
