9474d81ea60e6adea0b1c4f363d62c61fb0f75cc
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-04-24 11:56:51 +0900

    KAN-136 rename: add keyword atom

--- src/app/store/faviconRelativePosition.ts
+++
@@ -1,4 +0,0 @@
-import { atom } from 'jotai'
-
-// favicon position atom
-export const favicionPositionAtom = atom({ xRatio: 0.9, yRatio: 0.9 })
---
+++ src/app/store/faviconRelativePositionAtom.ts
@@ -0,0 +1,3 @@
+import { atom } from 'jotai'
+
+export const faviconRelativePositionAtom = atom({ xRatio: 0.9, yRatio: 0.8 })
