077f98742c82bb20d48dda099597b3d6f0d4678b
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-04-25 16:32:40 +0900

    KAN-136 refactor: fix chatbot absolute position

--- src/app/utils/computeChatbotPosition.ts
+++ src/app/utils/computeChatbotPosition.ts
@@ -1,15 +1,20 @@
 import { CHATBOT_DEFAULT_SIZE } from 'constants/chatbot/number'
 
-export const computeChatbotAbsolutePosition = (faviconX: number, faviconY: number) => {
+export const computeChatbotAbsolutePosition = (
+  faviconX: number,
+  faviconY: number,
+  innerWidth: number,
+  innerHeight: number,
+) => {
   let computedX, computedY
 
-  if (faviconX >= CHATBOT_DEFAULT_SIZE.width) {
+  if (faviconX + CHATBOT_DEFAULT_SIZE.width >= innerWidth) {
     computedX = faviconX - CHATBOT_DEFAULT_SIZE.width
   } else {
     computedX = faviconX
   }
 
-  if (faviconY >= CHATBOT_DEFAULT_SIZE.height) {
+  if (faviconY + CHATBOT_DEFAULT_SIZE.height >= innerHeight) {
     computedY = faviconY - CHATBOT_DEFAULT_SIZE.height
   } else {
     computedY = faviconY
