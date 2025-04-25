3c74ac584a9a3f771946f837f51ba2b2ec29ba10
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-04-25 16:38:48 +0900

    KAN-136 refactor: update chatbot to use relative size and enable dragging

--- src/app/components/chatbot-window/ChatbotWindow.tsx
+++ src/app/components/chatbot-window/ChatbotWindow.tsx
@@ -4,15 +4,16 @@
  */
 import Image from 'next/image'
 
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 
 import { CHATBOT_DEFAULT_SIZE } from 'constants/chatbot/number'
 import { AnimatePresence, motion } from 'framer-motion'
 import { useAtom, useAtomValue } from 'jotai'
 import { Direction } from 're-resizable/lib/resizer'
+import { DraggableEvent } from 'react-draggable'
 import { FiInfo } from 'react-icons/fi'
 import { IoClose } from 'react-icons/io5'
-import { Rnd } from 'react-rnd'
+import { DraggableData, Rnd } from 'react-rnd'
 import { chatbotAbsolutePositionAtom } from 'store/chatbotAbsolutePositionAtom'
 import { chatbotFixedMessageAtom } from 'store/chatbotFixedMessageAtom'
 import { chatbotRelativePositionAtom } from 'store/chatbotRelativePositionAtom'
@@ -21,6 +22,9 @@ import { isChatbotOpenAtom } from 'store/isChatbotOpenAtom'
 import ChatbotChatInput from '@components/chatbot-chat-input/ChatbotChatInput'
 import ChatbotMessageList from '@components/chatbot-message-list/ChatbotMessageList'
 import ExpandableContentBox from '@components/expandable-content-box/ExpandableContentBox'
+
+import { chatbotAbsoluteSizeAtom } from './../../store/chatbotAbsoluteSizeAtom'
+import { chatbotRelativeSizeAtom } from './../../store/chatbotRelativeSizeAtom'
 
 import classNames from 'classnames/bind'
 
@@ -31,32 +35,44 @@ const cx = classNames.bind(styles)
 type ResizeDirection = Direction
 
 export default function ChatbotWindow() {
-  const [chatbotSize, setChatbotSize] = useState(CHATBOT_DEFAULT_SIZE)
+  const innerWidth = window.innerWidth
+  const innerHeight = window.innerHeight
 
   const [isChatbotOpen, setIsChatbotOpen] = useAtom(isChatbotOpenAtom)
+
+  const [chatbotRelativeSize, setChatbotRelativeSize] = useAtom(chatbotRelativeSizeAtom)
+  const [chatbotRelativePosition, setChatbotRelativePosition] = useAtom(chatbotRelativePositionAtom)
+
+  const [chatbotAbsoluteSize, setChatbotAbsoluteSize] = useAtom(chatbotAbsoluteSizeAtom)
   const [chatbotAbsolutePosition, setChatbotAbsolutePosition] = useAtom(chatbotAbsolutePositionAtom)
 
-  const chatbotRelativePosition = useAtomValue(chatbotRelativePositionAtom)
   const chatbotFixedMessage = useAtomValue(chatbotFixedMessageAtom)
 
   useEffect(() => {
-    const updateChatbotPositionFromRatio = () => {
-      const x = chatbotRelativePosition.xRatio * window.innerWidth
-      const y = chatbotRelativePosition.yRatio * window.innerHeight
+    const updateSizeAndPositionFromRatio = () => {
+      setChatbotAbsoluteSize({
+        width: innerWidth * chatbotRelativeSize.widthRatio,
+        height: innerHeight * chatbotRelativeSize.heightRatio,
+      })
 
-      setChatbotAbsolutePosition({ x, y })
-      setChatbotSize({
-        width: CHATBOT_DEFAULT_SIZE.width,
-        height: CHATBOT_DEFAULT_SIZE.height,
+      setChatbotAbsolutePosition({
+        x: innerWidth * chatbotRelativePosition.xRatio,
+        y: innerHeight * chatbotRelativePosition.yRatio,
       })
     }
 
-    updateChatbotPositionFromRatio()
+    updateSizeAndPositionFromRatio()
+    window.addEventListener('resize', updateSizeAndPositionFromRatio)
 
-    window.addEventListener('resize', updateChatbotPositionFromRatio)
-
-    return () => window.removeEventListener('resize', updateChatbotPositionFromRatio)
-  }, [chatbotRelativePosition, setChatbotAbsolutePosition])
+    return () => window.removeEventListener('resize', updateSizeAndPositionFromRatio)
+  }, [
+    chatbotRelativeSize,
+    chatbotRelativePosition,
+    innerWidth,
+    innerHeight,
+    setChatbotAbsolutePosition,
+    setChatbotAbsoluteSize,
+  ])
 
   const handleResizeStop = (
     _e: MouseEvent | TouchEvent,
@@ -64,76 +80,114 @@ export default function ChatbotWindow() {
     ref: HTMLElement,
     delta: { width: number; height: number },
   ) => {
-    setChatbotSize({ width: ref.offsetWidth, height: ref.offsetHeight })
+    const newWidth = ref.offsetWidth
+    const newHeight = ref.offsetHeight
+
+    setChatbotAbsoluteSize({ width: newWidth, height: newHeight })
+
+    setChatbotRelativeSize({
+      widthRatio: newWidth / innerWidth,
+      heightRatio: newHeight / innerHeight,
+    })
 
     if (direction.includes('left') || direction.includes('top')) {
       setChatbotAbsolutePosition((prev) => {
         const newX = direction.includes('left') ? prev.x - delta.width : prev.x
         const newY = direction.includes('top') ? prev.y - delta.height : prev.y
+
+        setChatbotRelativePosition({
+          xRatio: newX / innerWidth,
+          yRatio: newY / innerHeight,
+        })
+
         return { x: newX, y: newY }
       })
     }
   }
 
+  const handleDragStop = (_: DraggableEvent, data: DraggableData) => {
+    setChatbotAbsolutePosition({
+      x: data.x,
+      y: data.y,
+    })
+
+    setChatbotRelativePosition({
+      xRatio: data.x / innerWidth,
+      yRatio: data.y / innerHeight,
+    })
+  }
+
   const handleCloseClick = () => setIsChatbotOpen(false)
 
   return (
-    <AnimatePresence>
+    <>
       {isChatbotOpen && (
         <Rnd
-          position={chatbotAbsolutePosition}
           bounds="window"
-          enableResizing={true}
-          disableDragging={true}
+          dragAxis="both"
+          dragHandleClassName="drag-handle"
+          enableResizing
+          position={chatbotAbsolutePosition}
+          size={{
+            width: chatbotAbsoluteSize.width,
+            height: chatbotAbsoluteSize.height,
+          }}
           default={{
             x: chatbotAbsolutePosition.x,
             y: chatbotAbsolutePosition.y,
-            width: CHATBOT_DEFAULT_SIZE.width,
-            height: CHATBOT_DEFAULT_SIZE.height,
+            width: chatbotAbsoluteSize.width,
+            height: chatbotAbsoluteSize.height,
           }}
-          size={{ width: chatbotSize.width, height: chatbotSize.height }}
+          minWidth={CHATBOT_DEFAULT_SIZE.width}
+          minHeight={CHATBOT_DEFAULT_SIZE.height}
           onResizeStop={handleResizeStop}
-          style={{ zIndex: 1000, position: 'fixed' }}
+          onDragStop={handleDragStop}
+          style={{
+            position: 'fixed',
+            zIndex: 1000,
+          }}
         >
-          <motion.div
-            className={cx('chatbot-window')}
-            initial={{ opacity: 0, y: 30 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -30 }}
-            transition={{ duration: 0.25 }}
-            style={{
-              width: '100%',
-              height: '100%',
-            }}
-          >
-            <div className={cx('chatbot-window__header')}>
-              <div className={cx('chatbot-window__header-content')}>
-                <p>챗봇</p>
-                {/* TODO: 아이콘 클릭 시 노션 페이지로 이동 */}
-                <button type="button">
-                  <FiInfo size={20} color="#CCCCCC" />
+          <AnimatePresence>
+            <motion.div
+              className={cx('chatbot-window')}
+              initial={{ opacity: 0, y: 30 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -30 }}
+              transition={{ duration: 0.25 }}
+              style={{
+                width: '100%',
+                height: '100%',
+              }}
+            >
+              <div className={cx('chatbot-window__header', { 'drag-handle': true })}>
+                <div className={cx('chatbot-window__header-content')}>
+                  <p>챗봇</p>
+                  {/* TODO: 아이콘 클릭 시 노션 페이지로 이동 */}
+                  <button type="button">
+                    <FiInfo size={20} color="#CCCCCC" />
+                  </button>
+                </div>
+                <button type="button" onClick={handleCloseClick}>
+                  <IoClose size={20} color="#1A1A1A" />
                 </button>
               </div>
-              <button type="button" onClick={handleCloseClick}>
-                <IoClose size={20} color="#1A1A1A" />
-              </button>
-            </div>
-            <div className={cx('chatbot-window__body')}>
-              {chatbotFixedMessage && (
-                <ExpandableContentBox
-                  leftIcon={<Image src="/icons/pin.svg" alt="고정" width={20} height={20} />}
-                >
-                  <p>{chatbotFixedMessage.content}</p>
-                </ExpandableContentBox>
-              )}
-              <ChatbotMessageList />
-            </div>
-            <div className={cx('chatbot-window__footer')}>
-              <ChatbotChatInput />
-            </div>
-          </motion.div>
+              <div className={cx('chatbot-window__body')}>
+                {chatbotFixedMessage && (
+                  <ExpandableContentBox
+                    leftIcon={<Image src="/icons/pin.svg" alt="고정" width={20} height={20} />}
+                  >
+                    <p>{chatbotFixedMessage.content}</p>
+                  </ExpandableContentBox>
+                )}
+                <ChatbotMessageList />
+              </div>
+              <div className={cx('chatbot-window__footer')}>
+                <ChatbotChatInput />
+              </div>
+            </motion.div>
+          </AnimatePresence>
         </Rnd>
       )}
-    </AnimatePresence>
+    </>
   )
 }
