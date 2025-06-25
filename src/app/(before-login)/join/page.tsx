dec5780c0581c7e855453fbfb1418d2ac3f6c85b
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-06-25 20:11:35 +0900

    KAN-173 feat: add redirect after successful signup

--- src/app/(before-login)/join/page.tsx
+++ src/app/(before-login)/join/page.tsx
@@ -68,7 +68,7 @@ export default function JoinPage() {
         referrer: document.referrer,
       })
       setIsCompleteJoin(true)
-      router.push('/login')
+      router.push('/')
     },
   })
 
