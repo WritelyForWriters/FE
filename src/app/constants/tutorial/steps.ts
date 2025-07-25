c44d795ef02127c5051d897b4b8900c99457e718
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-07-25 14:00:42 +0900

    KAN-178 refactor: define Joyride steps as constant outside component

--- src/app/constants/tutorial/steps.ts
+++ src/app/constants/tutorial/steps.ts
@@ -46,3 +46,55 @@ export const BEFORE_LOGIN_TUTORIAL_STEPS = [
     disableBeacon: true,
   },
 ]
+
+export const AFTER_LOGIN_TUTORIAL_STEPS = [
+  {
+    target: '.library-step-0',
+    content: '글쓰기 버튼을 클릭해보세요.',
+    placement: 'left' as Placement,
+    disableBeacon: true,
+  },
+  {
+    target: '.library-step-1',
+    content: '바로 작품 집필을 시작해요.',
+    placement: 'left' as Placement,
+    disableBeacon: true,
+    spotlightClicks: true,
+  },
+  {
+    target: '.library-step-2',
+    content: '어떤 작품을 쓸지 계획해요.',
+    placement: 'left' as Placement,
+    disableBeacon: true,
+    spotlightClicks: true,
+  },
+]
+
+export const PLANNER_TUTORIAL_STEPS = [
+  {
+    target: '.planner-step-1',
+    content: '어떤 장르의 이야기를 쓰고 싶으세요?',
+    placement: 'bottom' as Placement,
+    disableBeacon: true,
+  },
+  {
+    target: '.planner-step-2',
+    content:
+      '한 문장으로 쓰고 싶은 이야기를 들려주세요. \n 아이디어가 필요하면 오른쪽의 AI 버튼을 눌러 보세요.',
+    placement: 'bottom' as Placement,
+    disableBeacon: true,
+  },
+  {
+    target: '.planner-step-3',
+    content: '이제 저장만 하면 끝이에요!',
+    placement: 'bottom' as Placement,
+    disableBeacon: true,
+  },
+  {
+    target: '.planner-step-4',
+    content: '이제 집필하러 가볼까요?',
+    placement: 'bottom' as Placement,
+    disableBeacon: true,
+    spotlightClicks: true,
+  },
+]
